fbfaeff8379b5a3ec296abcecbfb72feec30b917
mattarr <mattarr@educ.college-montreal.qc.ca>
2022-11-09 10:46:34 -0500

    tp3

---
+++ TP_3.py
@@ -0,0 +1,55 @@
+import random
+nombre_vie = 0
+nombre_adversaire = 0
+nombre_victoire = 0
+nombre_defaites = 0
+nombre_victoire_suivi = 0
+force_adversaire = random. randint(1, 6) + random.randint(1, 6)
+
+def commcencement():
+
+    global nombre_vie, force_adversaire, nombre_victoire, nombre_defaites, nombre_victoire_suivi
+    nombre_vie = 0
+    force_adversaire = 0
+    nombre_victoire = 0
+    nombre_defaites = 0
+    nombre_victoire_suivi = 0
+    nombre_adversaire = 0
+
+
+def decision():
+
+    print(f"""votre niveau de vie est {nombre_vie}, 
+          votre adveesaire a une force de{force_adversaire}""")
+
+
+def menu():
+    choix = input(f" que voulez vous faire ? \n 1:combattre cet adversaire \n 2:contourner cet adversaire"
+                  "et ouvrir une autre porte \n 3:afficher les regles du jeu \n 4:quitter la parti  ")
+    if choix ==1 :
+        nombre_adversaire =+1
+        print(f"\n adversaire:{nombre_adversaire}"
+              f"\n force de l'adversaire {force_adversaire}"
+              f"\n votre nombre de vie {nombre_vie}"
+              f"combat{nombre_adversaire}:{nombre_victoire} victoire et {nombre_defaites} defaites")
+        lancer_de_1 = random.randint(1, 6)
+        lancer_de_2 = random.randint(1, 6)
+        score_de = lancer_de_1 + lancer_de_2
+        print(f"\n premier lancer de de : {lancer_de_1}"
+              f"\n deuxime lancer de de: {lancer_de_2}"
+              f"\n totale = {score_de}")
+        if score_de > force_adversaire:
+            resultat_combat= "victoire"
+            nombre_victoire_suivi += 1
+            nombre_victoire += 1
+            nombre_vie += force_adversaire
+            print(f"\n dernier combat : {resultat_combat}"
+                  f"votre nombre de vie : {nombre_vie}"
+                  f"nombre de victoire : {nombre_victoire_suivi}")
+            if nombre_victoire %3 == 0 and nombre_victoire != 0:
+                print("\n votre adversaire etait plus fort")
+
+
+
+
+
